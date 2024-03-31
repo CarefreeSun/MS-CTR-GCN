8631425649bd959b25543f4de2d8d3931c7714e9
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-03-31 20:09:32 +0800

    sh

--- ensemble_v2.sh
+++ ensemble_v2.sh
@@ -6,9 +6,3 @@ python ensemble_uav_v2.py \
 --bone-motion-dir ./weights/v2/bone-motion/ \
 --stream5 ./weights/v2/stream5/ \
 --stream6 ./weights/v2/stream6/
-# --joint-dir /mnt/netdisk/zhangjh/Code/CTR-GCN-main/work_dir/ntu60/xview/ctrgcn_joint/uavhuman_mix/v2/joint \
-# --bone-dir /mnt/netdisk/zhangjh/Code/CTR-GCN-main/work_dir/ntu60/xview/ctrgcn_joint/uavhuman_mix/v2/joint_bs \
-# --joint-motion-dir /mnt/netdisk/zhangjh/Code/CTR-GCN-main/work_dir/ntu60/xview/ctrgcn_joint/uavhuman_mix/v2/bone_bs \
-# --bone-motion-dir /mnt/netdisk/sunsf/Code/CTR-GCN-main/work_dir/uav/ctrgcn_joint_v2/ \
-# --stream5 /mnt/netdisk/sunsf/Code/CTR-GCN-main/work_dir/uav/ctrgcn_bone_v2  \
-# --stream6 /mnt/netdisk/sunsf/Code/CTR-GCN-main/work_dir/uav/ctrgcn_motion_v2
